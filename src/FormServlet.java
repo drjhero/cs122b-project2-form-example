f5a7b821c6253ed8eaec24ac7ba130ef70714c42
drjhero <james.thomas.herold@gmail.com>
2025-08-21 20:28:30 -0700

    Readability improvements, changed exception caught, reomved return statement from catch which prevented out.close()

--- src/FormServlet.java
+++ src/FormServlet.java
@@ -1,15 +1,17 @@
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
@@ -35,60 +37,37 @@ public class FormServlet extends HttpServlet {
     // Use http GET
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
-
         response.setContentType("text/html");    // Response mime type
-
         // Output stream to STDOUT
         PrintWriter out = response.getWriter();
-
         // Building page head with title
         out.println("<html><head><title>MovieDBExample: Found Records</title></head>");
-
         // Building page body
         out.println("<body><h1>MovieDBExample: Found Records</h1>");
 
-
-        try {
-
-            // Create a new connection to database
-            Connection dbCon = dataSource.getConnection();
-
-            // Declare a new statement
-            Statement statement = dbCon.createStatement();
-
-            // Retrieve parameter "name" from the http request, which refers to the value of <input name="name"> in index.html
+        try (Connection dbCon = dataSource.getConnection();
+             Statement statement = dbCon.createStatement()){
+            // Retrieve parameter "name" from the http request, which refers to the value of <input name="name"> in
+            // index.html
             String name = request.getParameter("name");
-
             // Generate a SQL query
             String query = String.format("SELECT * from stars where name like '%s'", name);
-
-
             // Log to localhost log
             request.getServletContext().log("query: " + query);
-
             // Perform the query
-            ResultSet rs = statement.executeQuery(query);
-            // Create a html <table>
-            out.println("<table border>");
-
-            // Iterate through each row of rs and create a table row <tr>
-            out.println("<tr><td>ID</td><td>Name</td></tr>");
-            while (rs.next()) {
-                String m_ID = rs.getString("ID");
-                String m_Name = rs.getString("name");
-                out.println(String.format("<tr><td>%s</td><td>%s</td></tr>", m_ID, m_Name));
+            try (ResultSet resultSet = statement.executeQuery(query)) {
+                // Create a html <table>
+                out.println("<table border>");
+                // Iterate through each row of resultSet and create a table row <tr>
+                out.println("<tr><td>ID</td><td>Name</td></tr>");
+                while (resultSet.next()) {
+                    String m_ID = resultSet.getString("ID");
+                    String m_Name = resultSet.getString("name");
+                    out.println(String.format("<tr><td>%s</td><td>%s</td></tr>", m_ID, m_Name));
+                }
+                out.println("</table>");
             }
-            out.println("</table>");
-
-
-            // Close all structures
-            rs.close();
-            statement.close();
-            dbCon.close();
-
-
-
-        } catch (Exception e) {
+        } catch (SQLException e) {
             /*
              * After you deploy the WAR file through tomcat manager webpage,
              *   there's no console to see the print messages.
@@ -101,8 +80,10 @@ public class FormServlet extends HttpServlet {
             request.getServletContext().log("Error: ", e);
 
             // Output Error Message to html
-            out.println(String.format("<html><head><title>MovieDBExample: Error</title></head>\n<body><p>SQL error in doGet: %s</p></body></html>", e.getMessage()));
-            return;
+            out.println(String.format(
+                    "<html><head><title>MovieDBExample: Error</title></head>\n" +
+                            "<body><p>SQL error in doGet: %s</p></body></html>",
+                    e.getMessage()));
         }
         out.close();
     }
